4eaccf3d61d4cd8f2edccfe46b3647c91f6dbf2b
Pablo202212 <112069904+Pablo202212@users.noreply.github.com>
2024-04-11 21:11:36 +0200

    Update Patient.cs

--- Krankenhausinformationssystem/Model/Patient.cs
+++ Krankenhausinformationssystem/Model/Patient.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Krankenhausinformationssystem.Krankenhausinformationssystem.cs
+namespace Krankenhausinformationssystem
 {
     internal class Patient
     {
-
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+        public string Geschlecht { get; set; }
+        public string Adresse { get; set; }
+        public string GetPatientenInfo()
+        {
+            return $"Patient ID: {PatientId}, Name: {Name}, Geburtsdatum: {Geburtsdatum}, Geschlecht: {Geschlecht}, Adresse: {Adresse}";
+        }
     }
 }
