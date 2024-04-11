6a36f19d39de9324aee6a40cbc81246363ab5a6d
Parsa Faraji <parsa.f3@gso.schule.koeln>
2024-04-11 21:07:53 +0200

    Aufgabe gearbeitet

---
+++ Krankenhausinformationssystem/Daten/KrankenhausContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Krankenhausinformationssystem.Daten
+{
+    internal class KrankenhausContext:DbContext
+    {
+        // DbSet-Eigenschaften repräsentieren Tabellen in der Datenbank
+        // Jede Eigenschaft ist stark typisiert mit einer Modellklasse
+        public DbSet<Kunde> Kunden { get; set; } = null!;
+        public DbSet<Bestellung> Bestellungen { get; set; } = null!;
+        public DbSet<Produkt> Produkte { get; set; } = null!;
+        public DbSet<BestellungDetail> BestellungDetails { get; set; } = null!;
+
+        // OnModelCreating-Methode wird verwendet, um das Modell und die Beziehungen mittels Fluent API zu konfigurieren
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Definieren von Primärschlüsseln für jede Entität
+            modelBuilder.Entity<Kunde>().HasKey(k => k.Id);
+            modelBuilder.Entity<Bestellung>().HasKey(b => b.Id);
+            modelBuilder.Entity<Produkt>().HasKey(p => p.Id);
+            modelBuilder.Entity<BestellungDetail>().HasKey(bd => bd.Id);
+
+            // Immer die Basis-Methode aufrufen, um das Basisverhalten einzuschließen
+            base.OnModelCreating(modelBuilder);
+        }
+
+        // OnConfiguring-Methode wird verwendet, um die Datenbankverbindung und andere Konfigurationen einzustellen
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Überprüfen, ob der optionsBuilder bereits konfiguriert ist, wenn nicht, konfigurieren
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Konfigurieren der Verwendung der SQLite-Datenbank mit dem angegebenen Verbindungsstring
+                optionsBuilder.UseSqlite(@"Data Source=C:\Users\parsa\source\repos\Krankenhausinformationssystem\Krankenhausinformationssystem\krankenhaus.db");
+            }
+
+            // Immer die Basis-Methode aufrufen, um das Basisverhalten einzuschließen
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Krankenhausinformationssystem/Model/Behandlung.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Krankenhausinformationssystem
+{
+    internal class Behandlung
+    {
+        public int BehandlungsId { get; set; }
+        public int PatientId { get; set; }
+        public string Diagnose { get; set; }
+        public string Medikation { get; set; }
+        public DateTime BehandlungsDatum { get; set; }
+        public string Arzt { get; set; }
+    }
+}
